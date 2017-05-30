d387cda8ed1abbc095b2998ec232371bfa230a89
Raihan <raihan.dk@gmail.com>
2017-05-31 00:24:28 +0600

    accessing shared ui module from child

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -5,10 +5,12 @@ import {RouterModule} from "@angular/router";
 import {userRoutes} from "./user-routes";
 import {ListUserComponent} from "./pages/list-user/list-user.component";
 import {AddUserComponent} from "./pages/add-user/add-user.component";
+import {UiModule} from "../../ui/ui.module";
 
 @NgModule({
     imports: [
         CommonModule,
+        UiModule,
         RouterModule.forChild(userRoutes)
     ],
     declarations: [UserComponent, ListUserComponent, AddUserComponent]
